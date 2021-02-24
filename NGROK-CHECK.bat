c51c5e568b54b07be971b1035ea94d887a989202
Thượng Hải <haithuongpro2711@gmail.com>
2021-02-24 18:14:24 +0700

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -6,7 +6,7 @@ net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo NGROK Services is not Running
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret"
 echo User: Administrator
 echo Pass: Thuonghai001
 
