900c8ac31361e80af3c7141bc7b8c744ff85e840
Thượng Hải <haithuongpro2711@gmail.com>
2021-02-25 04:54:38 +0700

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -12,6 +12,7 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " & ping 127.0.0.1 >Nul
 echo User: Administrator
 echo Pass: Thuonghai001
+ping -n 10 127.0.0.1 >nul
 
 
 
