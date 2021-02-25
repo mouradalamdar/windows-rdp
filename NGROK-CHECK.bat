1fc28438816bc4c954ec042ba4e00e18dcbc43f7
Thượng Hải <haithuongpro2711@gmail.com>
2021-02-25 15:51:15 +0700

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -7,6 +7,8 @@ sc start audiosrv >nul
 net user administrator Thuonghai001 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " & ping 127.0.0.1 >Nul
