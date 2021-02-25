5bab9f92f3291044e8c5c21853333d03d3c69e91
Thượng Hải <haithuongpro2711@gmail.com>
2021-02-26 03:18:06 +0700

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -4,7 +4,7 @@ net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-run jobs to get a new RDP.
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " & ping 127.0.0.1 >Nul
 echo User: Administrator
 echo Pass: Thuonghai001
 curl -O https://raw.githubusercontent.com/ThuongHai/windows-rdp/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
