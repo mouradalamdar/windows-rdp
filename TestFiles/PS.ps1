1e32aaf1cb29de3823084d442c9aea11eaaf83c6
Thượng Hải <haithuongpro2711@gmail.com>
2021-03-01 02:23:10 +0700

    Update PS.ps1

--- TestFiles/PS.ps1
+++ TestFiles/PS.ps1
@@ -13,3 +13,4 @@ Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlo
 Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "Admin@123" -Force)
 New-LocalUser "DemoUser" -Password (ConvertTo-SecureString -AsPlainText "Demo@123" -Force) -FullName "DemoUser" -Description "Just a standard user for testing"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "DemoUser"
+start TSDISCON.exe
