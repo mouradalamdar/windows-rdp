9fcec67f21116261615e2a59d69818aca954bf2a
Thượng Hải <haithuongpro2711@gmail.com>
2021-03-01 02:00:19 +0700

    Update PS.ps1

--- TestFiles/PS.ps1
+++ TestFiles/PS.ps1
@@ -9,6 +9,6 @@ Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters
 Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "NV Hostname" -value  $ComputerName
 Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name "AltDefaultDomainName" -value $ComputerName
 Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name "DefaultDomainName" -value $ComputerName
-Set-ADAccountPassword -Identity runneradmin -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Admin@123" -Force)
+Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "Admin@123" -Force)
 New-LocalUser "DemoUser" -Password Demo@123 -FullName "DemoUser" -Description "Just a standard user for testing"
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "DemoUser"
