8374a2cb63bed46b7bfd79b98a4206a6342d663c
Thượng Hải <haithuongpro2711@gmail.com>
2021-03-01 01:48:08 +0700

    Create PS.ps1

---
+++ TestFiles/PS.ps1
@@ -0,0 +1,14 @@
+$ComputerName = "DemoVM"
+   
+Remove-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "Hostname" 
+Remove-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "NV Hostname" 
+
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\Computername" -name "Computername" -value $ComputerName
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Control\Computername\ActiveComputername" -name "Computername" -value $ComputerName
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "Hostname" -value $ComputerName
+Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -name "NV Hostname" -value  $ComputerName
+Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name "AltDefaultDomainName" -value $ComputerName
+Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -name "DefaultDomainName" -value $ComputerName
+Set-ADAccountPassword -Identity runneradmin -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "Admin@123" -Force)
+New-LocalUser "DemoUser" -Password Demo@123 -FullName "DemoUser" -Description "Just a standard user for testing"
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member "DemoUser"
