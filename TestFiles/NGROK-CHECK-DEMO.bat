fafd4f98420b6b63534d4b4cd49c020e20bf3112
Thượng Hải <haithuongpro2711@gmail.com>
2021-03-01 03:03:49 +0700

    Update NGROK-CHECK-DEMO.bat

--- TestFiles/NGROK-CHECK-DEMO.bat
+++ TestFiles/NGROK-CHECK-DEMO.bat
@@ -11,6 +11,7 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
+cls
 echo All done! Connect your VM using RDP. This is a Demo Session.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
